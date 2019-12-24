f28686ed4c5b84cc8724a94e3e17f0f217c4aead
tomoya-onuki <tomoya.onuki28@gmail.com>
2019-12-24 14:52:36 +0900

    photo変更

---
+++ js/photos.js
@@ -0,0 +1,6 @@
+var img_size = 18;
+for (var i = 0; i < img_size; i++) {
+  var html = '<a href="./img/top/'+i+'.jpeg" class="photo_img_size">'
+              +'<img src="./img/top/'+i+'.jpeg" style="width:100%;"></a>';
+  document.write(html);
+}
