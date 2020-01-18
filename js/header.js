206e9f5eefaa6539b22e8f22bea778c31e6c303a
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-01-19 00:27:46 +0900

    画像のパスを貼り直す

--- js/header.js
+++ js/header.js
@@ -1,2 +1,2 @@
-var head_str = '<div name="top" class="header">\n<div class="head_font">\n<a href="../">TOMOYA ONUKI</a>\n<span class="head_menu">\n<a class ="link" href="../works/" style="margin-left:1vw">WORKS</a>\n<a class ="link" href="../profile.html" style="margin-left:1vw">PROFILE</a>\n</span>\n</div>\n</div>';
+var head_str = '<div name="top" class="header">\n<div class="head_text"><a class="head_font" href="../">TOMOYA ONUKI</a>\n<span class="head_menu">\n<a class ="link" href="../works/" style="margin-left:1vw">WORKS</a>\n<a class ="link" href="../profile.html" style="margin-left:1vw">PROFILE</a>\n</span></div>\n</div>';
 document.write(head_str);
