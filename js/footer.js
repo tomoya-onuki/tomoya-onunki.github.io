03f106d01d2e31088a8a592e45bcc717a178adf8
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-01-22 12:50:31 +0900

    works/indexの画像のサイズ(スマホ用)の修正

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,3 @@
-var foot_str = '<center><div style="font-size: 80%; margin-top:30px; margin-bottom:80px;" class="main">\n<p>©️2017 - 2020 小貫智弥 | Tomoya Onuki</p>\n</div></center>';
+var foot_str = '<center><div style="font-size: 70%; margin-top:20px; margin-bottom:50px;" class="main">\n<p>©️2017 - 2020 小貫智弥 | Tomoya Onuki</p>\n</div></center>';
 
 document.write(foot_str);
