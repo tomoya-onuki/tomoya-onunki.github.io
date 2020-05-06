cbcd15f72d271923df4856ca56a1df0584c740ec
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-05-06 19:04:16 +0900

    画像変換JSを生成するPHPを作成

---
+++ get_img_name.php
@@ -0,0 +1,29 @@
+<?php
+$result = glob('./img/index/*');
+$jsArray = "var fname = [";
+foreach ($result as $fname ) {
+  // array_push($array, '"'.$fname.'"');
+  $jsArray .= '"'.$fname.'",';
+}
+$jsArray .= "];\n";
+$jsArray2 = str_replace('""', '", "', $jsArray);
+
+
+$ctx2 = <<<EOD
+var max = 23;
+var count = Math.floor(Math.random() * max);
+imgTimer();
+
+function imgTimer() {
+  document.image_place.src = fname[count];
+  count++;
+  if (count > fname.size()) count = 0;
+  setTimeout("imgTimer()",1000); //*6
+}
+EOD;
+
+
+$f = fopen("./js/index_img.js", "w");
+fwrite($f, $jsArray2.$ctx2);
+fclose($f);
+?>
