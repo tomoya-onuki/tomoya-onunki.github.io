e8340312ee7709f47ec53112b0baef4ba97dc521
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-05-08 18:25:56 +0900

    画像削除

--- js/index_img.js
+++ js/index_img.js
@@ -1,4 +1,4 @@
-var fname = ["./img/index/0.JPG","./img/index/1.JPG","./img/index/10.JPG","./img/index/11.JPG","./img/index/12.JPG","./img/index/13.JPG","./img/index/14.JPG","./img/index/15.JPG","./img/index/16.JPG","./img/index/17.JPG","./img/index/18.JPG","./img/index/2.JPG","./img/index/3.JPG","./img/index/4.JPG","./img/index/5.JPG","./img/index/6.JPG","./img/index/7.JPG","./img/index/8.JPG","./img/index/9.JPG","./img/index/DSC03531.JPG","./img/index/DSC04365.JPG","./img/index/IMG_6163.JPG","./img/index/LRG_DSC00229.JPG","./img/index/LRG_DSC03913.JPG",];
+var fname = ["./img/index/0.JPG","./img/index/1.JPG","./img/index/10.JPG","./img/index/11.JPG","./img/index/12.JPG","./img/index/13.JPG","./img/index/14.JPG","./img/index/15.JPG","./img/index/16.JPG","./img/index/18.JPG","./img/index/2.JPG","./img/index/3.JPG","./img/index/4.JPG","./img/index/5.JPG","./img/index/6.JPG","./img/index/7.JPG","./img/index/8.JPG","./img/index/9.JPG","./img/index/DSC03531.JPG","./img/index/DSC04365.JPG","./img/index/LRG_DSC03913.JPG",];
 var max = fname.length;
 var delta = 1;
 var count = Math.floor(Math.random() * max);
