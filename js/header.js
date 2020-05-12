5ed21180dd5c5a50969fa5b266155cd239307342
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-05-12 17:43:12 +0900

    メンテナンス中

--- js/header.js
+++
@@ -1,2 +0,0 @@
-var head_str = '<div name="top" class="header">\n<div class="head_text"><a target="_self" class="head_font" href="../">TOMOYA ONUKI</a>\n<span class="head_menu">\n<a target="_self" class ="link" href="../works/" style="margin-left:1vw">WORKS</a>\n<a target="_self" class ="link" href="../profile.html" style="margin-left:1vw">PROFILE</a>\n</span></div>\n</div>';
-document.write(head_str);
