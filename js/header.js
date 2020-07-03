d876c480e8165501326cfe3004ced72ca905d877
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-07-03 23:19:12 +0900

    前のバージョンを保管

--- js/header.js
+++ js/header.js
@@ -1,25 +1,2 @@
-// var head_str = '<div name="top" class="header">\n<div class="head_text"><a target="_self" class="head_font" href="../">TOMOYA ONUKI</a>\n<span class="head_menu">\n<a target="_self" class ="link head_menu_font" href="../works/">WORKS</a>\n<span class="head_menu_font"> / </span>\n<a target="_self" class ="link head_menu_font" href="../profile.html">ABOUT</a>\n</span></div>\n</div>';
-
-//ヒアドキュメント
-head_str = (function () {/*
-<div name="top" class="header">
-  <div class="head_text">
-    <a target="_self" class="head_font" href="../">TOMOYA ONUKI</a>
-    <span class="head_menu">
-      <a id="works" target="_self" class ="link head_menu_font" href="../works/" onclick="worksBtn()">WORKS</a>
-      <span class="head_menu_font"> / </span>
-      <a id="about" target="_self" class ="link head_menu_font" href="../profile.html" onclick="aboutBtn()">ABOUT</a>
-    </span>
-  </div>
-</div>
-*/}).toString().match(/(?:\/\*(?:[\s\S]*?)\*\/)/).pop().replace(/^\/\*/, "").replace(/\*\/$/, "");
+var head_str = '<div name="top" class="header">\n<div class="head_text"><a target="_self" class="head_font" href="../">TOMOYA ONUKI</a>\n<span class="head_menu">\n<a target="_self" class ="link head_menu_font" href="../works/">WORKS</a>\n<span class="head_menu_font"> / </span>\n<a target="_self" class ="link head_menu_font" href="../profile.html">PROFILE</a>\n</span></div>\n</div>';
 document.write(head_str);
-
-function worksBtn() {
-  document.getElementById("works").style.color ="#F43E43";
-  document.getElementById("about").style.color ="#1f1f1f";
-}
-function aboutBtn() {
-  document.getElementById("works").style.color ="#1f1f1f";
-  document.getElementById("about").style.color ="#F43E43";
-}
