e1d87fc52b94d1ee6ea19a370f79d1037080e053
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-09-20 19:14:44 +0900

    new

--- js/main_content_laod.js
+++ js/main_content_laod.js
@@ -1,7 +1,7 @@
 var html = new Map();
 html.set('performing-arts-tamago',
 `<div class="works_contents" name="performing-arts-tamago">
-    <h3><span class="highlight">Performing Arts Tamago Vol.22</h3>
+    <h3><span class="highlight">Performing Arts Tamago Vol.22</span></h3>
     <div>2020.06 / 撮影・編集 / ChoreoVideo<br>
     企画・制作：筑波大学舞踊研究室<br>
     協力：筑波大学ダンス部 <a href="http://tsa.tsukuba.ac.jp/dance/" target="_blank"><u>http://tsa.tsukuba.ac.jp/dance/</u></a></div>
@@ -13,7 +13,7 @@ html.set('performing-arts-tamago',
 
 html.set('mawase-groove-making',
 `<div class="works_contents" name="mawase-groove-making">
-    <h3><span class="highlight">【MVメイキング映像】「回せ！」回せ！グルーヴ開発部</h3>
+    <h3><span class="highlight">【MVメイキング映像】「回せ！」回せ！グルーヴ開発部</span></h3>
     <div>2020.04 / 撮影・編集 / MakingMoive<br>
     企画：回せ！グルーヴ開発部 <a href="http://mawaguru.jp" target="_blank"><u>http://mawaguru.jp</u></a></div>
     <div class="mov">
@@ -24,7 +24,7 @@ html.set('mawase-groove-making',
 
 html.set('dancing-inside-tribute',
 `<div class="works_contents" name="dancing-inside-tribute">
-      <h3><span class="highlight">うちで踊ろう</h3>
+      <h3><span class="highlight">うちで踊ろう</span></h3>
       <div>2020.04 / 監督・撮影・編集 / TributeFilm<br>
       music:Gen Hoshino</div>
       <div class="mov">
@@ -35,7 +35,7 @@ html.set('dancing-inside-tribute',
 
 html.set('express',
 `<div class="works_contents" name="express">
-    <h3><span class="highlight">EXPRESS 筑波大学ダンス部</h3>
+    <h3><span class="highlight">EXPRESS 筑波大学ダンス部</span></h3>
     <div>2020.01 / 監督・撮影・編集 / Documentary Movie<br>
     制作：Sta2one <a href="https://www.instagram.com/sta2one" target="_blank"><u>https://www.instagram.com/sta2one</u></a><br>
     出演：筑波大学ダンス部 <a href="http://tsa.tsukuba.ac.jp/dance/" target="_blank"><u>http://tsa.tsukuba.ac.jp/dance/</u></a><br>
@@ -50,7 +50,7 @@ html.set('express',
 html.set('yuragi',
 `<div class="works_contents" name="yuragi">
 
-    <h3><span class="highlight">ゆらぎ</h3>
+    <h3><span class="highlight">ゆらぎ</span></h3>
     <div>
       2019.11 / 監督・撮影・編集 / Digest Movie<br>
       制作：TParty <a href="https://twitter.com/tparty_tsukuba" target="_blank"><u>https://twitter.com/tparty_tsukuba</u></a><br>
@@ -67,7 +67,7 @@ html.set('yuragi',
 
 html.set('tparty-launch',
 `<div class="works_contents" name="tparty-launch">
-    <h3><span class="highlight">Tparty Launch Party / Digest Video</h3>
+    <h3><span class="highlight">Tparty Launch Party / Digest Video</span></h3>
     <div>
       2019.06 / 監督・撮影・編集 / Digest Movie<br>
       制作：TParty <a href="https://twitter.com/tparty_tsukuba" target="_blank"><u>https://twitter.com/tparty_tsukuba</u></a><br>
@@ -82,7 +82,7 @@ html.set('tparty-launch',
 html.set('monoshiritori-making',
 `<div class="works_contents" name="monoshiritori-making">
 
-    <h3><span class="highlight">モノシリトリ / MakingMoive</h3>
+    <h3><span class="highlight">モノシリトリ / MakingMoive</span></h3>
     <div>2017.10 / 監督・撮影・編集 / Digest Movie</div>
     <div class="mov">
       <iframe  src="https://www.youtube.com/embed/videoseries?list=PLODDdOOKFDnIpvey6zz47Af9-RZXRZjz5" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
