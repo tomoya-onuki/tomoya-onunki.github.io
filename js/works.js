2b4913f06c5ae6bec24a71fd8c786f7152e968b7
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-09-25 17:33:08 +0900

    ヘッダ

---
+++ js/works.js
@@ -0,0 +1,28 @@
+// var works = [
+//   {
+//     id    : 'performing-arts-tamago',
+//     title : '「つくば駅→筑波大学　4種のダンス共に」Performing Arts Tamago Vol.22',
+//     info  : {
+//       date  : '2020.06',
+//       genre : 'Choreo Video',
+//       role  : [
+//         'Cinematographer',
+//         'Editor'
+//       ]
+//     },
+//     contents : {
+//       movurl : 'https://www.youtube.com/embed/32P2vfq96-I',
+//       text   : [
+//         {
+//           title : null,
+//           text  : 'Performing Arts Tamago」は、ダンス部を引退した大学院生が自由に踊ったり、作品を発表する場が少ない背景から自主的に始まった企画です。毎年形を変えて継続的に身体表現の魅力を伝え続けています。22回目である2020年は「つくば駅→筑波大学　4種のダンスと共に」をコンセプトに映像作品として進化。つくば駅から始まり、道中で異なるスタイルのダンスパフォーマンスと出会いながら筑波大学までガイドしていきます。'
+//         },{
+//           title : null,
+//           text  : 'Cinematographer, Editor : Tomoya Onuki <br>
+//           Producer : 筑波大学舞踊研究室 <a href="http://tsa.tsukuba.ac.jp/dance/" target="_blank"><u>http://tsa.tsukuba.ac.jp/dance/</u></a><br>
+//           Cast : Risako Shibata / Asuki Nakanishi / Chiharu Hayami / Yui Miyakawa / Mei Yamamoto'
+//         }
+//       ]
+//     }
+//   }
+// ];
