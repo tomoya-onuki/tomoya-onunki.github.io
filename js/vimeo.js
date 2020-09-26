7d280c0d5a0c56bcbe7a5fff5bd71681b4fe23b3
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-09-27 00:37:22 +0900

    ajax test2

--- js/vimeo.js
+++ js/vimeo.js
@@ -1,15 +1,15 @@
-// window_load();  //読み込み時の表示
-// window.onresize = window_load;  //ウィンドウサイズ変更時に更新
-//
-// //サイズの表示
-// function window_load() {
-//   $('#reel_player').width(window.innerWidth * 0.95);
-//   $('#reel_player').height(window.innerHeight * 0.95);
-// }
-//
-//
-// // 一時停止
-// var player = new Vimeo.Player('reel_player');
-// function pause() {
-//   player.pause();
-// }
+window_load();  //読み込み時の表示
+window.onresize = window_load;  //ウィンドウサイズ変更時に更新
+
+//サイズの表示
+function window_load() {
+  $('#reel_player').width(window.innerWidth * 0.95);
+  $('#reel_player').height(window.innerHeight * 0.95);
+}
+
+
+// 一時停止
+var player = new Vimeo.Player('reel_player');
+function pause() {
+  player.pause();
+}
