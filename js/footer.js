aff1358b52133d1c17c9bf68c4c27572a31a63b6
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-09-26 21:54:33 +0900

    ajaxなしに戻した

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+var foot_str = `
+  <center>
+    <div style="font-size: 70%; margin-top:40px; margin-bottom:20px;" class="main">
+      <p>(C) 2017 - 2020 Tomoya Onuki</p>
+    </div>
+  </center>`;
+document.write(foot_str);
