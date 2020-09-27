55a7ba3e68022518a54d9e539cad1dcbd4056eb9
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-09-27 16:12:48 +0900

    ajax test

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,2 @@
-var foot_str = `
-  <center>
-    <div style="font-size: 70%; margin-top:40px; margin-bottom:20px;" class="main">
-      <p>(C) 2017 - 2020 Tomoya Onuki</p>
-    </div>
-  </center>`;
+var foot_str = `<p>(C) 2017 - 2020 Tomoya Onuki</p>`;
 document.write(foot_str);
