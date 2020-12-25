6f476cf5f32f525358586ed1707e670bb1dbad33
tomoya-onuki <tomoya.onuki28@gmail.com>
2020-12-25 19:48:25 +0900

    hoverイベントの変更

---
+++ js/touch_nonhover.js
@@ -0,0 +1,18 @@
+var touch = 'ontouchstart' in document.documentElement || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+
+if(touch) {
+  try {
+    for (var si in document.styleSheets) {
+      var styleSheet = document.styleSheets[si];
+      if (!styleSheet.rules) continue;
+
+      for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+        if (!styleSheet.rules[ri].selectorText) continue;
+
+        if (styleSheet.rules[ri].selectorText.match(':hover')) {
+          styleSheet.deleteRule(ri);
+        }
+      }
+    }
+  } catch (ex) {}
+}
