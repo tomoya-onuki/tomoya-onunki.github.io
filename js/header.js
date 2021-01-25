3d192ccb3ffcbf27a97c8b7f9e84eb581015ab7a
tomoya-onuki <tomoya.onuki28@gmail.com>
2021-01-25 17:56:54 +0900

    新バージョンにアップデート

---
+++ js/header.js
@@ -0,0 +1,13 @@
+function printHeader(worksUrl, aboutUrl) {
+  var header_str = `
+  <div class="header2 head_font1">
+    <a id="top" class="head_logo" href="./">TOMOYAONUKI.</a>
+    <span class="head_menu">
+      <a href="`+worksUrl+`">WORKS</a> / <a href="`+aboutUrl+`">ABOUT</a>
+    </span>
+    <span id="hamb_menu">
+      <div></div><div></div><div></div>
+    </span>
+  </div>`;
+  document.write(header_str);
+}
