67508dee9c1ffc85147d8b1fd066f4e9ba388178
tomoya-onuki <tomoya.onuki28@gmail.com>
2021-01-25 18:10:36 +0900

    リンクが変

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 function printHeader(worksUrl, aboutUrl) {
   var header_str = `
   <div class="header2 head_font1">
-    <a id="top" class="head_logo" href="./">TOMOYAONUKI.</a>
+    <a id="top" class="head_logo" href="https://tomoya-onuki.github.io">TOMOYAONUKI.</a>
     <span class="head_menu">
       <a href="`+worksUrl+`">WORKS</a> / <a href="`+aboutUrl+`">ABOUT</a>
     </span>
