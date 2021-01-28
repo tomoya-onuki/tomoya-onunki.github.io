6f03cee0b263f69b2667197d826b1c6fd00fa208
tomoya-onuki <tomoya.onuki28@gmail.com>
2021-01-29 01:34:07 +0900

    works/indexのスクロール情報をセッションで保管し再現する

--- js/session.js
+++ js/session.js
@@ -1,10 +1,12 @@
 $(function(){
   var top_y = window.sessionStorage.getItem(['y']);
-  scrollTo(0, top_y);
+  if(top_y != null){
+    scrollTo(0, top_y);
+  }
   window.sessionStorage.removeItem(['y']);
 
-  $('a').each(function(){
-    $(this).on('click', function(){
+  $('.contents').each(function(){
+    $(this).children('a').on('click', function(){
       top_y = window.pageYOffset;
       window.sessionStorage.setItem(['y'],[top_y]);
     });
