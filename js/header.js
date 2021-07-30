1b569191136f0fffa4b82b620fc49909fb1fbfab
tomoya-onuki <tomoya.onuki28@gmail.com>
2021-07-30 17:12:03 +0900

    デザインを微修正

--- js/header.js
+++ js/header.js
@@ -10,13 +10,25 @@ function printHeader(worksUrl, aboutUrl, reelUrl) {
 
     <div class="mobile">
       <div class="head_menu">
-        <div>TOMOYAONUKI.</div>
+        <div>ONUKITOMOYA.</div>
         <hr>
         <div><a href="https://tomoya-onuki.github.io">TOP</a></div>
         <div><a href="`+worksUrl+`">WORKS</a></div>
+        <div style="font-size:80%; margin-top:-30px;">
+          <a href="`+worksUrl+`#mov">movie</a> / <a href="`+worksUrl+`#art">art</a> / <a href="`+worksUrl+`#app">app</a>
+        </div>
+        <div style="font-size:80%; margin-top:-35px;">
+          <a href="https://tomoya-onuki.github.io/photo">photo</a> / <a href="https://tomoya-onuki.github.io/reel">reel</a>
+        </div>
         <div><a href="`+aboutUrl+`">PROFILE</a></div>
+        <hr>
+
+        <div class="footer" style="font-size:20%;">
+              <script type="text/javascript"> printFooter(); </script>
+        </div>
       </div>
     </div>
+  
 
     <span id="hamb_menu">
       <div></div><div></div><div></div>
