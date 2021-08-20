343e84ab9b2c1c8a758810ed482aad5b12f95ce0
tomoya-onuki <tomoya.onuki28@gmail.com>
2021-08-20 23:55:48 +0900

    new ver

--- works/admin/xml2html.py
+++ works/admin/xml2html.py
@@ -160,7 +160,7 @@ def generateLinks(tempStr, contentsName, cTree):
         str = '<a id="prev" href="'+prev+'.html"></a>'
         tempStr = tempStr.replace('<!-- prev -->', str)
 
-    str = '<a id="close" href="./#'+contentsName+'"></a>'
+    str = '<a id="close" href="../#'+contentsName+'"></a>'
     tempStr = tempStr.replace('<!-- close -->', str)
     return tempStr
 
