6da64ace66d732769072cae66769ec4948473de1
tomoya-onuki <tomoya.onuki28@gmail.com>
2021-08-23 23:03:39 +0900

    デザイン修正

--- js/header.js
+++ js/header.js
@@ -1,20 +1,25 @@
-function printHeader(worksUrl, aboutUrl, reelUrl) {
-  
+function printHeader(worksUrl, profUrl) {
+  var worksLink = '<a href="'+worksUrl+'">works</a>';
+  var profLink = '<a href="'+profUrl+'">profile</a>';
+
+  if (!worksUrl || worksUrl === '../index.html') {
+    worksLink = '<a href="'+worksUrl+'" class="underbar">works</a>';
+  }
+  if (!profUrl) {
+    var profLink = '<a href="'+profUrl+'" class="underbar">profile</a>';
+  }
+
   var header_str = `
-  <div class="header2 head_font1">
-    <a id="top" class="head_logo" href="https://tomoya-onuki.github.io">ONUKITOMOYA.</a>
+  <div class="header2">
+    <a id="top" class="head_font title" href="`+worksUrl+`">onukitomoya.</a>
     <div class="pc">
-      <span class="head_menu">
-          <a href="`+worksUrl+`">WORKS</a> / <a href="`+aboutUrl+`">PROFILE</a>
-      </span>
+      <span class="head_menu head_font">`+worksLink+` / `+profLink+`</span>
     </div>
 
     <div class="mobile">
       <div class="head_menu">
-        <div>ONUKITOMOYA.</div>
-        <hr>
-        <div><a href="`+worksUrl+`">WORKS</a> / <a href="`+aboutUrl+`">PROFILE</a></div>
-
+        <div>onukitomoya.</div>
+        <div class="head_font">`+worksLink+` / `+profLink+`</div>
         <div class="footer" style="font-size:20%;">
               <script type="text/javascript"> printFooter(); </script>
         </div>
