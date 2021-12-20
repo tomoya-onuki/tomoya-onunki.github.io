40ac8a9e084314ea408b8785bbd2f4fee0b8c7f9
tomoya-onuki <tomoya.onuki28@gmail.com>
2021-12-21 00:34:29 +0900

    switch

--- js/css_switch.js
+++ js/css_switch.js
@@ -1,14 +1,15 @@
 
 $(function () {
-    
+
     // smart phone
     if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1)
         || navigator.userAgent.indexOf('iPod') > 0
         || navigator.userAgent.indexOf('Android') > 0) {
 
-        $('.flex_box .mask').css('opacity','1');
+        $('.flex_box .mask').css('opacity', '0.9');
+        $('.flex_box .info').css('opacity', '0.5');
 
-    } 
+    }
     // PC
     else {
 
