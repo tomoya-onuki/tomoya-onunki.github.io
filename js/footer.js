ddde5c76b0a27871e32a4ab4a335a632dc007589
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-01-01 18:02:20 +0900

    2022

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
 function printFooter() {
-  var foot_str = `<p>(C) 2017 - 2021 小貫智弥 / Tomoya Onuki</p>`;
+  var foot_str = `<p>(C) 2017 - 2022 小貫智弥 / Tomoya Onuki</p>`;
   document.write(foot_str);
 }
