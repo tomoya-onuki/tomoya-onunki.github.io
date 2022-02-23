e9aeba107c4b1381a3b915ef512d655680c47144
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-23 23:01:48 +0900

    show more

--- js/hidden.js
+++ js/hidden.js
@@ -10,10 +10,11 @@ $(function() {
 
         if (i == 9) {
             $(this).after(`
+            <h2><a id="allView" style="text-align: center;cursor:pointer"> show more</a></h2>
             <div class="fill flex_box"></div>
             <div class="fill flex_box"></div>
             <div class="fill flex_box"></div>
-            <h2><a id="allView" style="text-align: center;cursor:pointer"> show more</a></h2>`);
+            `);
         }
         i++;
     });
